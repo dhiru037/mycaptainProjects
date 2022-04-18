3b479037ec85491c99859a96b92884d67c10dc86
Dhiruram B <93209461+dhiru037@users.noreply.github.com>
2022-04-18 19:43:37 +0530

    Update project1B.py

--- project1B.py
+++ project1B.py
@@ -1,14 +1,14 @@
 filename=input("Input the Filename")
 name, extension=filename.split('.')
-if extension=='.py':
+if extension=='py':
     print("The extension of the file is: python")
-elif extension=='.c':
+elif extension=='c':
     print("The extension of the file is: c")
-elif extension=='.java':
+elif extension=='java':
     print("The extension of the file is: java")
-elif extension=='.js':
+elif extension=='js':
     print("The extension of the file is: java script")
-elif extension=='.html':
+elif extension=='html':
     print("The extension of the file is: HTML")
 else:
     print("Some other extension")
